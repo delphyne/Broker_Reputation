6aafb765ffb31ed5ccf797e6ff76bc1131b42ed5
Brian M. Carr <delphyne@gmail.com>
2010-01-15 20:14:20 -0600

    refactored options menu creation, fixed initial gains/losses value, removed player_login event

--- Broker_Reputation.lua
+++ Broker_Reputation.lua
@@ -1,7 +1,9 @@
 local Factions = CreateFrame("Frame", "Broker_Reputation")
 local FactionsMenu = CreateFrame("Frame", "FactionsMenu")
 
-local rep_starting_value
+local rep_starting_value = 0
+local init_done = false
+local last_faction = nil
 local gender = UnitSex("player")
 
 Factions.obj = LibStub("LibDataBroker-1.1"):NewDataObject("Broker_Reputation", {
@@ -33,36 +35,36 @@ end
 local function getLabelText(name, standingID, barMin, barMax, barValue, full)
     local labelText = ""
     if name then
-        if Broker_ReputationDB.show_faction_name or full then
+        if Broker_ReputationDB["Show Faction Name"] or full then
             labelText = labelText .. name .. ": "
         end
         
         local current = barValue - barMin
         local goal = barMax - barMin
         
-        if Broker_ReputationDB.show_rep_value or full then
+        if Broker_ReputationDB["Show Progress"] or full then
             local calculated_text = current .. "/" .. goal
-            if Broker_ReputationDB.show_colors or full then
+            if Broker_ReputationDB["Use Color"] or full then
                 labelText = labelText .. colorize(calculated_text, standingID)
             else
                 labelText = labelText .. calculated_text
             end
         end
         
-        if Broker_ReputationDB.show_percentage or full then
+        if Broker_ReputationDB["Show Percentage"] or full then
            labelText = labelText .. " (" .. floor(((current / goal) + .005) * 100) .. "%)"
         end
         
-        if Broker_ReputationDB.show_rep_level or full then
+        if Broker_ReputationDB["Show Standing"] or full then
             calculated_text = " " .. GetText("FACTION_STANDING_LABEL" .. standingID, gender)
-            if Broker_ReputationDB.show_colors or full then
+            if Broker_ReputationDB["Use Color"] or full then
                 labelText = labelText .. colorize(calculated_text, standingID)
             else
                 labelText = labelText .. calculated_text
             end
         end
         
-        if Broker_ReputationDB.show_difference or full then
+        if Broker_ReputationDB["Show Gains/Losses"] or full then
             local diff = barValue - rep_starting_value
             labelText = labelText .. " "
             if diff >= 0 then
@@ -81,6 +83,12 @@ end
 -- updates the label text
 local function update()
     local name, standingID, barMin, barMax, barValue = GetWatchedFactionInfo()
+    
+    if not (name == last_faction) then
+        rep_starting_value = barValue
+        last_faction = name
+    end
+
     Factions.obj.text = getLabelText(name, standingID, barMin, barMax, barValue)
 end
 
@@ -91,20 +99,16 @@ local function handleFactionEvent(self, event, ...)
         if arg1 == "Broker_Reputation" then
             if Broker_ReputationDB == nil then
                 Broker_ReputationDB = {
-                    show_faction_name = true,
-                    show_rep_value = true,
-                    show_percentage = true,
-                    show_rep_level = true,
-                    show_difference = true,
-                    show_colors = true
+                    ["Show Faction Name"] = true,
+                    ["Show Progress"] = true,
+                    ["Show Percentage"] = true,
+                    ["Show Standing"] = true,
+                    ["Show Gains/Losses"] = true,
+                    ["Use Color"] = true
                 }
             end
         end
-    else 
-        if event == "PLAYER_LOGIN" then
-            local name, standingID, barMin, barMax, barValue = GetWatchedFactionInfo()
-            rep_starting_value = barValue
-        end
+    else
         update()
     end
 end
@@ -143,50 +147,20 @@ FactionsMenu.initialize = function(self, level)
         info.disabled = nil
         info.isTitle = nil
         info.notCheckable = nil
-       
-        info.text = "Faction name"
-        info.func = function()
-            Broker_ReputationDB.show_faction_name = not Broker_ReputationDB.show_faction_name
-            update()
-        end
-        info.checked = Broker_ReputationDB.show_faction_name
-        UIDropDownMenu_AddButton(info, level)
         
-        info.text = "Numbers"
-        info.func = function()
-            Broker_ReputationDB.show_rep_value = not Broker_ReputationDB.show_rep_value
-            update()
+        -- build a menu item for each option
+        for k,v in pairs(Broker_ReputationDB) do
+            info.text = k
+            info.func = function()
+                Broker_ReputationDB[k] = not Broker_ReputationDB[k]
+                update()
+            end
+            info.checked = Broker_ReputationDB[k]
+            UIDropDownMenu_AddButton(info, level)
         end
-        info.checked = Broker_ReputationDB.show_rep_value
-        UIDropDownMenu_AddButton(info, level)
-        
-        info.text = "Percentage"
-        info.func = function()
-            Broker_ReputationDB.show_percentage = not Broker_ReputationDB.show_percentage
-            update()
-        end
-        info.checked = Broker_ReputationDB.show_percentage
-        UIDropDownMenu_AddButton(info, level)
-        
-        info.text = "Reputation Level"
-        info.func = function()
-            Broker_ReputationDB.show_rep_level = not Broker_ReputationDB.show_rep_level
-            update()
-        end
-        info.checked = Broker_ReputationDB.show_rep_level
-        UIDropDownMenu_AddButton(info, level)
-        
-        info.text = "Show Gain/Loss"
-        info.func = function()
-            Broker_ReputationDB.show_difference = not Broker_ReputationDB.show_difference
-            update()
-        end
-        info.checked = Broker_ReputationDB.show_difference
-        UIDropDownMenu_AddButton(info, level)
     end
 end
 
 Factions:SetScript("OnEvent", handleFactionEvent)
 Factions:RegisterEvent("UPDATE_FACTION")
-Factions:RegisterEvent("PLAYER_LOGIN")
 Factions:RegisterEvent("ADDON_LOADED")
